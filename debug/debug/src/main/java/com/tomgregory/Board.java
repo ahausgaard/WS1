1f2fa45b2d722b67d2488b4df6dbe3b8cdbe230b
Andreas Jakobsen <ahausgaardjakobsen@gmail.com>
2025-11-13 19:52:45 +0100

    First commit

---
+++ debug/debug/src/main/java/com/tomgregory/Board.java
@@ -0,0 +1,96 @@
+package com.tomgregory;
+
+import java.awt.*;
+import java.util.Random;
+
+public class Board {
+    private static final int BATTLESHIP_LENGTH = 3;
+    private final boolean[][] grid;
+    private int hitCount = 0;
+
+    public Board(int gridSize) {
+        grid = new boolean[gridSize][gridSize];
+        addBattleship();
+    }
+
+    public boolean attack(Point attackPoint) {
+        if (grid[attackPoint.x][attackPoint.y]) {
+            hitCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isSunk() {
+        return hitCount >= BATTLESHIP_LENGTH;
+    }
+
+    public void reveal() {
+        System.out.println("Revealing battleship location");
+        StringBuilder gridVisualisation = new StringBuilder();
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid.length; x++) {
+                if (grid[x][y]) {
+                    gridVisualisation.append("x ");
+                } else {
+                    gridVisualisation.append("o ");
+                }
+            }
+            gridVisualisation.append(System.lineSeparator());
+        }
+        System.out.println(gridVisualisation);
+    }
+
+    public int getGridSize() {
+        return grid.length;
+    }
+
+    private void addBattleship() {
+        Point startPoint;
+        Point endPoint;
+
+        do {
+            startPoint = randomGridPoint();
+            endPoint = calculateEndPoint(startPoint);
+        } while (!isValidPoint(endPoint));
+
+        for (int x = startPoint.x; x < endPoint.x + 1; x++) {
+            for (int y = startPoint.y; y < endPoint.y + 1; y++) {
+                grid[x][y] = true;
+            }
+        }
+    }
+
+    private boolean isValidPoint(Point pt) {
+        return pt.x < grid.length && pt.y < grid.length;
+    }
+
+    private Point randomGridPoint() {
+        return new Point(getRandomInteger(grid.length - 1), getRandomInteger(grid.length - 1));
+    }
+
+    private Point calculateEndPoint(Point startPoint) {
+      int choice = getRandomInteger(1);
+      switch (choice) {
+        case 0:
+          return getHorizontalEndPoint(startPoint);
+        case 1:
+          return getVerticalEndPoint(startPoint);
+        default:
+          throw new IllegalStateException("Unexpected value: " + choice);
+      }
+
+    }
+
+    private Point getHorizontalEndPoint(Point startPoint) {
+        return new Point(startPoint.x + BATTLESHIP_LENGTH - 1, startPoint.y);
+    }
+
+    private Point getVerticalEndPoint(Point startPoint) {
+        return new Point(startPoint.x, startPoint.y + BATTLESHIP_LENGTH - 1);
+    }
+
+    private static int getRandomInteger(int maxValue) {
+        return new Random().nextInt(maxValue + 1);
+    }
+}
